be0a6f8adfd07e9d2f23f62c0c60bdc3786df29f
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-15 22:38:31 +0200

    enumerate e insert + finalizar

--- ej3.py
+++ ej3.py
@@ -1,7 +1,18 @@
 
-
+valor = [5,2,5,6,8,1,4,5,9]
 
 def modificar(valor):
     
     lista = list(set(valor))
     lista.sort()
+
+    for i, c in enumerate(lista):
+        if (lista[i]%2)!=0:
+            lista.remove(c)
+
+    lista.insert(0, sum(lista))
+    return lista
+
+n_lista = modificar(valor)
+
+print(n_lista[0]==sum(n_lista[1:]))
