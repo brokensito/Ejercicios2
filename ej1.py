0a88f4be5c2a94bc7ce93cdf7100610b213513e8
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-15 18:09:12 +0200

    vector, distancia

--- ej1.py
+++ ej1.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Punto:
 
     def __init__(self,valor_x = 0, valor_y = 0):
@@ -16,3 +19,23 @@ class Punto:
 
         elif self.valor_x==0 and self.valor_y==0:
             return "Esta situado sobre el ORIGEN"
+
+        elif self.valor_x>0 and self.valor_y>0:
+            return "Esta en el PRIMER cuadrante"
+
+        elif self.valor_x<0 and self.valor_y>0:
+            return "Esta en el SEGUNDO cuadrante"
+
+        elif self.valor_x<0 and self.valor_y<0:
+            return "Esta en el TERCER cuadrante"
+
+        elif self.valor_x>0 and self.valor_y<0:
+            return "Esta en el CUARTO cuadrante"
+
+    def vector(self, punto2):
+        vector = (punto2.punto()[0]-self.valor_x, punto2.punto()[1]-self.valor_y)
+        return vector
+    
+    def distancia(self, punto2):
+        distancia = math.sqrt(((punto2.punto()[0]-self.valor_x)**2)+((punto2.punto()[1]-self.valor_y)**2))
+        return distancia
