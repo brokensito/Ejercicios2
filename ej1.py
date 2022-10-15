8aed28e988ca82d67fee2d6a45a07cdccfb4041a
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-15 17:40:27 +0200

    constructor, innit

---
+++ ej1.py
@@ -0,0 +1,8 @@
+class Punto:
+
+    def __init__(self,valor_x = 0, valor_y = 0):
+        self.valor_x = valor_x
+        self.valor_y = valor_y
+
+    def punto(self):
+        return (self.valor_x,self.valor_x)
