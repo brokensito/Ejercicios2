62d631f222f48e0f32b7660b7372d52afc1be232
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-15 17:56:57 +0200

    cuadrantes

--- ej1.py
+++ ej1.py
@@ -6,3 +6,13 @@ class Punto:
 
     def punto(self):
         return (self.valor_x,self.valor_x)
+
+    def cuadrante(self):
+        if self.valor_x==0 and self.valor_y!=0:
+            return "Esta situado sobr el eje Y"
+        
+        elif self.valor_x!=0 and self.valor_y==0:
+            return "Esta situado sobre el eje X"
+
+        elif self.valor_x==0 and self.valor_y==0:
+            return "Esta situado sobre el ORIGEN"
