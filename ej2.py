53ccf71f6be407471debd39c4be6813d9ffaa336
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-15 20:35:23 +0200

    split y capitalize

---
+++ ej2.py
@@ -0,0 +1,10 @@
+
+import enum
+
+
+texto = "un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
+
+nuevo = texto.split("#")
+
+for i, c in enumerate(nuevo):
+    nuevo[i] = c.capitalize()
